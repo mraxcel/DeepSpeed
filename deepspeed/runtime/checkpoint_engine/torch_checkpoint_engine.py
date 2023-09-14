442be9ecc0e980654eb22fb91c87294a7a388580
axcel <axcel@me.com>
2023-09-14 15:38:35 +0100

    [torch_checkpoint_engine] custom fs support via fsspec

--- deepspeed/runtime/checkpoint_engine/torch_checkpoint_engine.py
+++ deepspeed/runtime/checkpoint_engine/torch_checkpoint_engine.py
@@ -7,7 +7,8 @@ import torch
 from deepspeed.utils import logger, log_dist
 from deepspeed.runtime.checkpoint_engine.checkpoint_engine import \
     CheckpointEngine
-
+import fsspec
+import io
 
 class TorchCheckpointEngine(CheckpointEngine):
 
@@ -17,15 +18,22 @@ class TorchCheckpointEngine(CheckpointEngine):
     def create(self, tag):
         log_dist(f"[Torch] Checkpoint {tag} is about to be saved!", ranks=[0])
 
+    def makedirs(self, path, exist_ok=False):
+        fsspec.makedirs(path, exist_ok=True)
+
     def save(self, state_dict, path: str):
         logger.info(f"[Torch] Saving {path}...")
-        torch.save(state_dict, path)
+        bytesbuffer = io.BytesIO()
+        torch.save(state_dict, bytesbuffer)
+        with fsspec.open(path, "wb") as f:
+            f.write(bytesbuffer.getvalue())
         logger.info(f"[Torch] Saved {path}.")
         return None
 
     def load(self, path: str, map_location=None):
         logger.info(f"[Torch] Loading checkpoint from {path}...")
-        partition = torch.load(path, map_location=map_location)
+        with fsspec.open(path, "rb") as f:
+            partition = torch.load(f, map_location=map_location)
         logger.info(f"[Torch] Loaded checkpoint from {path}.")
         return partition
 
